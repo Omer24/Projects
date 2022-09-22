0af6517c5f5c4ce03cd0388b79947d716bf70b5c
Ömer Akyol <88924759+Omer24@users.noreply.github.com>
2022-09-22 20:39:00 +0300

    C#101(Patika)

---
+++ C#101(Patika)/dongular-while-foreach.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace dongular_while_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // //While
+            // //1'den Başlayarak Console'dan Girilen Sayıya Kadar (sayı dahil) Ortalama Hesaplayıp Console'a Yazdıran Program.
+            // Console.Write("Lütfen Bir Sayi Giriniz: ");
+            // int sayi = int.Parse(Console.ReadLine());
+            // int sayac = 1;
+            // int toplam = 0;
+            // while (sayac <= sayi)
+            // {
+            //     toplam += sayac;
+            //     sayac ++;
+            // }
+            // Console.WriteLine(toplam/sayi);
+
+            // // A'dan Z'ye Kadar Tüm Harfleri Console'a Yazdır.
+            // char character = 'a';
+            // while (character < 'z')
+            // {
+            //     Console.Write(character);
+            //     character ++;
+            // }
+
+            Console.Write("***** Foreach *****");
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
